c6629ba0c56869adfb9b066c99926c368f975362
Josh Long <josh.long@springsource.com>
2011-10-13 00:08:28 -0700

    updated configuration .. trying to make it all work again..

--- src/test/java/org/springframework/batch/core/partition/gemfire/BatchConfiguration.java
+++ src/test/java/org/springframework/batch/core/partition/gemfire/BatchConfiguration.java
@@ -1,30 +1,29 @@
 package org.springframework.batch.core.partition.gemfire;
 
 
+import com.gemstone.gemfire.cache.Region;
 import org.apache.commons.dbcp.BasicDataSource;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.partition.PartitionHandler;
+import org.springframework.batch.core.partition.support.SimplePartitioner;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.item.SimpleStepFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-import java.io.InputStream;
-import java.io.Reader;
 
 @Configuration
 @ImportResource("/META-INF/spring/theone.xml")
@@ -33,7 +32,11 @@ public class BatchConfiguration {
 
     @Autowired private Environment environment;
 
-    @Autowired @Qualifier("jobRepository") private JobRepository jobRepository;
+    @Autowired @Qualifier("region")
+    private Region<String, StepExecution> region;
+
+    @Autowired @Qualifier("jobRepository")
+    private JobRepository jobRepository;
 
     @Value("classpath:/org/springframework/batch/core/schema-drop-h2.sql")
     private Resource h2Cleanup;
@@ -56,8 +59,8 @@ public class BatchConfiguration {
         cleaner.setIgnoreFailedDrops(true);
         cleaner.setContinueOnError(true);
 
-        ResourceDatabasePopulator databasePopulator = new  ResourceDatabasePopulator();
-        databasePopulator.addScript( this.h2DatabasePopulatorResource );
+        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+        databasePopulator.addScript(this.h2DatabasePopulatorResource);
         databasePopulator.setContinueOnError(true);
 
         DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
@@ -70,15 +73,8 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager() throws Exception {
+    public PlatformTransactionManager transactionManager() {
         return new DataSourceTransactionManager(this.dataSource());
-    }
-
-    @Bean
-    public SimpleJobLauncher jobLauncher() {
-        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
-        jobLauncher.setJobRepository(this.jobRepository);
-        return jobLauncher;
     }
 
     @Bean
@@ -91,5 +87,36 @@ public class BatchConfiguration {
         return basicDataSource;
     }
 
+    @Bean
+    public SimpleJobLauncher jobLauncher() {
+        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+        jobLauncher.setJobRepository(this.jobRepository);
+        return jobLauncher;
+    }
+
+    @Bean
+    @Scope("remote")
+    public SimpleStepFactoryBean step() {
+        SimpleStepFactoryBean<String, Object> bean = new SimpleStepFactoryBean<String, Object>();
+        bean.setItemReader(new ExampleItemReader());
+        bean.setItemWriter(new ExampleItemWriter());
+        bean.setTransactionManager(this.transactionManager());
+        bean.setJobRepository(this.jobRepository);
+        return bean;
+    }
+
+    @Bean
+    public PartitionHandler handler() throws Exception {
+        GemfirePartitionHandler gemfirePartitionHandler = new GemfirePartitionHandler();
+        gemfirePartitionHandler.setGridSize(2);
+        gemfirePartitionHandler.setStep((Step) step().getObject());
+        gemfirePartitionHandler.setRegion(this.region);
+        return gemfirePartitionHandler;
+    }
+
+    @Bean
+    public SimplePartitioner partitioner() {
+        return new SimplePartitioner();
+    }
 
 }
